dce833aa094d752508b441962ed290bb930e623b
LaloFl <119621269+DiegoFlCB@users.noreply.github.com>
2023-11-15 09:52:38 -0800

    index layout

--- src/components/pages/Deals/index.tsx
+++ src/components/pages/Deals/index.tsx
@@ -1,5 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 
 export const Deals = component$(({ data }: { data: any }) => {
-  return <div>Deals</div>;
+  const dealsData = data["pageDeals"]["data"]["attributes"]["Deals"];
+  const introData = data["pageDeals"]["data"]["attributes"]["RefIntro"];
+  return (
+    <div
+      onClick$={() => {
+        console.log(data);
+      }}
+    >
+      Deals
+    </div>
+  );
 });
