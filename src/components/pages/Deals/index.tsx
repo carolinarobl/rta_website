c08bbb6f695f469d25e16fcb538338953cfd57ef
LaloFl <119621269+DiegoFlCB@users.noreply.github.com>
2023-11-15 11:15:56 -0800

    feat: intro deals

--- src/components/pages/Deals/index.tsx
+++ src/components/pages/Deals/index.tsx
@@ -1,15 +1,23 @@
 import { component$ } from "@builder.io/qwik";
+import { SectionDeals } from "./SectionDeals";
+import { SectionDIntro } from "./SectionDIntro";
 
 export const Deals = component$(({ data }: { data: any }) => {
   const dealsData = data["pageDeals"]["data"]["attributes"]["Deals"];
   const introData = data["pageDeals"]["data"]["attributes"]["RefIntro"];
+  const discountsData = data["pageDeals"]["data"]["attributes"]["Discounts"];
+  const disclaimer = data["pageDeals"]["data"]["attributes"]["Disclaimer"];
   return (
     <div
       onClick$={() => {
         console.log(data);
       }}
     >
-      Deals
+      <SectionDeals data={dealsData} />
+      <SectionDIntro data={introData} />
+      <div class="flex w-full items-center  justify-center p-8 text-center text-[14px] italic text-primary-dark-blue">
+        {disclaimer}
+      </div>
     </div>
   );
 });
