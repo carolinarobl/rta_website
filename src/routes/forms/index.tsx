3ae9fdbea449c79a73f8346b5ec40bdff0dfa8fe
Fernando Cano <fernado.cano@cbluna.com>
2023-11-15 09:29:01 -0800

    Creacion de componenete download-box-image

--- src/routes/forms/index.tsx
+++ src/routes/forms/index.tsx
@@ -1,6 +1,7 @@
 import { QRL, component$, $ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { InitialValues,SubmitHandler,email, minLength, required, useForm } from '@modular-forms/qwik';
+import { DownloadBoxImage } from '~/components/download-box-image';
 import { FormCarrers } from '~/components/forms/form-carrers';
 import { FormPosition } from '~/components/forms/form-position';
 import { FormSupport } from '~/components/forms/form-support';
@@ -8,6 +9,7 @@ import { Test1 } from '~/components/forms/test';
 
 export default component$(() => {
   return <>
+  <DownloadBoxImage></DownloadBoxImage>
   <FormCarrers/>
   <FormPosition></FormPosition>
   <FormSupport></FormSupport>
