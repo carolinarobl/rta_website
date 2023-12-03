46b8894ddb3ebdcdbe2948f072fc7bac93fb6774
lynatical <43594257+lynatical@users.noreply.github.com>
2023-12-03 01:14:00 -0800

    Se restaura Winners Circle temp para build

--- src/components/pages/WinnersCircle/index.tsx
+++ src/components/pages/WinnersCircle/index.tsx
@@ -1,91 +1,96 @@
 import { component$ } from "@builder.io/qwik";
-import Carousel from "~/components/Carousel";
-import { StrapiImage } from "~/components/StrapiImage";
-import { formatDate, setURL } from "~/data/constants";
+import { Link } from "@builder.io/qwik-city";
+import { StrapiImage } from "./StrapiImage";
+import { FaBarsSolid } from "@qwikest/icons/font-awesome";
 
-export const WinnersCircle = component$(({ data }: { data: any }) => {
-  const pageData = data["pageWinnersC"]["data"]["attributes"];
-
+export const Navbar = component$(({ data }: { data: any }) => {
   return (
-    <div
-      onClick$={() => {
-        console.log(pageData);
-      }}
-      class="relative flex w-full flex-col items-center justify-center"
-    >
-      <div class="relative flex w-full items-center justify-center overflow-hidden">
-        <StrapiImage
-          width="1024"
-          height="603"
-          clasN="max-w-[1000px] w-fit z-10 object-cover"
-          url={pageData["WinnersBG"]["data"]["attributes"]["url"]}
-        />
-        <div class="absolute z-20 mb-[160px] h-[370px] w-[290px] overflow-hidden  rounded-[10px] border border-gray-500 bg-gray-900 p-2">
-          <div
-            class={`h-full w-full overflow-hidden rounded-[10px] bg-cover `}
-            style={{
-              backgroundImage: `url(${setURL(
-                pageData["Winners"]["data"][1]["attributes"]["url"],
-              )})`,
-            }}
-          ></div>
-        </div>
-        <div class="absolute bottom-0 left-0 right-0 h-[30%] bg-black"></div>
+    <div class="sticky top-0 z-30 shadow-[0_5px_40px_-25px_rgba(0,0,0,0.2)]">
+      <div class="flex h-[28px] flex-row items-center justify-end gap-4 bg-primary-blue px-5 py-[4px] text-[12px] font-bold text-white max-sm:justify-evenly max-sm:text-[10px] max-sm:font-normal max-[400px]:text-[9px]">
+        {data["TopOptions"].map((e: any, i: any) => {
+          // TEMP FIX
+          // TEMP FIX
+          // TEMP FIX
+          if (e["Link"].includes("=p")) {
+            e["Link"] = "";
+          }
+
+          return (
+            <Link key={i} href={e["Link"]} prefetch>
+              <div class="flex flex-row items-center gap-2">
+                <div class="w-[12px]">
+                  <StrapiImage
+                    url={e["Icon"]["data"]["attributes"]["url"]}
+                    toWhite={true}
+                    width="16"
+                    height="16"
+                  />
+                </div>
+                {e["Text"]}
+              </div>
+            </Link>
+          );
+        })}
       </div>
-      <div class="flex max-w-[1200px] gap-4 pt-16 max-[1000px]:flex-col">
-        <div class="flex w-[30%] flex-col gap-4">
-          <h2 class="text-center text-[36px] font-[600] leading-[36px] text-secondary-red">
-            {pageData["GiveawayCarTitle"]}
-          </h2>
-          <div class="mb-6 flex items-center justify-center rounded-xl bg-white p-2">
-            <Carousel
-              id={"giveAnswers"}
-              hasPagination={false}
-              slidesQty={1}
-              duration={5000}
-              addSpace={true}
-              hasArrows={false}
-              slides={pageData["GiveawayAnswers"].map((ans: any, i: number) => {
-                return (
-                  <div
-                    class="flex flex-col items-center justify-center gap-2"
-                    key={i}
-                  >
-                    <p class="text-center text-[15px] text-primary-blue">
-                      This giveaway question was answered on{" "}
-                      {formatDate(ans["AnswerDate"], false)}
-                    </p>
-                    <iframe
-                      src={`https://www.youtube.com/embed/${
-                        ans["QuestionVideos"].split("v=")[1]
-                      }`}
-                      class="rounded-[30px] shadow-md"
-                      frameBorder="0"
-                      height={280}
-                      width={160}
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      allowFullScreen
-                    />
-                    <span class="text-secondary-red">Answer:</span>
-                    <span class="text-primary-blue">{ans["Answer"]}</span>
-                  </div>
-                );
-              })}
+      <div class="grid grid-cols-3 items-center justify-evenly bg-white py-1 min-[1200px]:flex">
+        <div class="flex items-center justify-center min-[1200px]:hidden">
+          <FaBarsSolid class="min-[1200px]:hidden" />
+        </div>
+        <div class="flex items-center justify-center">
+          <div class="w-[125px] py-1">
+            <StrapiImage
+              url={data["MainMenu"]["Logo"]["data"]["attributes"]["url"]}
+              width={774}
+              height={282}
             />
           </div>
         </div>
-        <div class="flex w-[70%] flex-col gap-4">
-          <h3 class="text-center text-[28px] font-[500] leading-[30px] text-primary-blue">
-            {pageData["TwitterFeedTitle"]}
-          </h3>
-          <iframe
-            class="h-[500px]"
-            srcDoc={pageData["TwitterFeedLink"]}
-            width={"100%"}
-            height={"100%"}
-            frameborder="0"
-          ></iframe>
+        {/* Main options */}
+        <div class="flex items-center justify-center gap-4 max-[1200px]:hidden">
+          {data["MainOptions"].map(function (e: any, i: any) {
+            const hasSubOptions = e["SubOption"].length > 0;
+            if (hasSubOptions) {
+              //
+            }
+
+            return (
+              <div
+                key={i}
+                class="bg-primary-blue bg-clip-text text-[15px] font-bold text-transparent"
+              >
+                {!hasSubOptions ? (
+                  <Link href={e["Link"]}>{e["Text"]}</Link>
+                ) : (
+                  <></>
+                )}
+              </div>
+            );
+          })}
         </div>
+
+        <div class="flex items-center justify-center">
+          {/* <LangSwitch data={data["MainMenu"]["Switcher"]} /> */}Langswitch
+        </div>
+      </div>
+      <div class="flex h-[32px] flex-row items-center justify-center gap-6 bg-secondary-red py-[2px] text-[14px] font-semibold tracking-[1px] text-white max-sm:text-[11px]">
+        {data["ClientOptions"].map((e: any, i: any) => (
+          <Link key={i} href={e["Link"]} prefetch>
+            {e["Text"]}
+          </Link>
+        ))}
+      </div>
+      <div class="flex flex-row items-center justify-evenly bg-white py-1 max-[1200px]:hidden">
+        {data["gigfastOptions"].map((e: any, i: any) => (
+          <Link key={i} href={e["Link"]} prefetch>
+            <div class="w-[130px] rounded-md p-1 hover:bg-slate-50">
+              <StrapiImage
+                url={e["Icon"]["data"]["attributes"]["url"]}
+                width="311"
+                height="60"
+              />
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
