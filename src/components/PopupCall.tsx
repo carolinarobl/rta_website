da443a310389e53611898069e087bfc5a7e1ce8c
lynatical <43594257+lynatical@users.noreply.github.com>
2023-12-03 01:00:12 -0800

    Actualizar PopupCall.tsx x2

--- src/components/PopupCall.tsx
+++ src/components/PopupCall.tsx
@@ -2,32 +2,43 @@ import { component$, useSignal } from "@builder.io/qwik";
 import { FormCarrers } from "./forms/form-carrers";
 import { PortabilityRequest } from "./popups/portabilityRequest";
 import { FormSupport } from "./forms/form-support";
+
 export const PopupCall = component$(({ link }: { link: string, text: string }) => {
-const configurator = "=pConf=";
+  const configurator = "=pConf=";
   const contactEmail = "=pContactEmail=";
   const iFrame = "=pIFrame=";
   const channelLineup = "=pChannelLineup=";
+
   // var popupCall = link;
   let child = null
+
   if (link.includes(configurator)) {
     // popupCall = configurator;
     child = <FormCarrers />
+
     // link = link.replaceAll(configurator, "");
   } else if (link.includes(contactEmail)) {
     // popupCall = contactEmail;
     child = <FormSupport />
-} else if (link.includes(iFrame)) {
+
+    // link = link.replaceAll(contactEmail, "");
+  } else if (link.includes(iFrame)) {
     const linkClean = link.replaceAll(iFrame, "");
     child = <PortabilityRequest link={linkClean} />
   } else if (link.includes(channelLineup)) {
     // popupCall = channelLineup;
     child = <FormCarrers />
+
     // link = link.replaceAll(channelLineup, "");
   }
+
+
   const showModal = useSignal(false);
 
+
   return <div class="flex relative">
-{/* {link.includes(configurator) && text.includes("Buy now" || "Comprar ahora") ? <div class="my-4 cursor-pointer flex h-[80px] w-full flex-col items-center justify-center">
+
+    {/* {link.includes(configurator) && text.includes("Buy now" || "Comprar ahora") ? <div class="my-4 cursor-pointer flex h-[80px] w-full flex-col items-center justify-center">
       <div class="border-gary-500 my-4 h-[1px] w-full border-t-2"></div>
       <div class="flex h-[50px] w-full flex-row items-center justify-center rounded-full border-2 border-teal-500 bg-transparent p-1 px-6 text-btn-green hover:bg-teal-500 hover:text-white"
         onClick$={() => showModal.value = true}>
@@ -37,7 +48,7 @@ const configurator = "=pConf=";
         </div>
       </div>
     </div> : <button class={`flex w-fit items-center justify-center gap-2 ${link.includes(channelLineup) ? "" : "border-2 border-teal-500 p-1 px-7 rounded-full shadow-md transition-all  hover:text-white hover:cursor-pointer hover:border-transparent hover:bg-teal-500"}  bg-white  text-btn-green opacity-80  text-[15px] font-[600] max-md:text-[14px] max-sm:text-[13px] `}
-onClick$={() => showModal.value = true}>{text}</button>} */}
+      onClick$={() => showModal.value = true}>{text}</button>} */}
 
 
     {showModal.value && (
