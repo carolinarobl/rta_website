b59533785079fdb357f5242630270ac8daf0521c
lynatical <carolinaroblero8@gmail.com>
2023-12-13 21:07:22 -0800

    Versión para producción

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,7 +4,7 @@ import { type DocumentHeadValue } from "@builder.io/qwik-city";
 export const strapiURL = "https://strapi.rtatel.com";
 export const strapiGQLURL = "http://10.5.24.41:1337";
 
-export const gqlURL = `${strapiURL}/graphql`;
+export const gqlURL = `${strapiGQLURL}/graphql`;
 
 export const setURL = (url: string) => {
   return `${strapiURL}${url}`;
--- src/root.tsx
+++ src/root.tsx
@@ -13,14 +13,24 @@ import { translationFn } from "./speak-functions";
 import "./global.css";
 
 export default component$(() => {
-  const analyticsScriptP1 = `https://www.googletagmanager.com/gtag/js?id=G-73FN8JZFH3`;
+  const analyticsScriptP1 = `https://www.googletagmanager.com/gtag/js?id=G-ZSBXBFHTVV`;
 
-const analyticsScriptP2 = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-73FN8JZFH3');
-`;
+  const analyticsScriptP2 = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-ZSBXBFHTVV');
+  `;
+
+  const gtmScriptP1 = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W5BJSW4');`;
+
+
+
+
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
@@ -32,19 +42,22 @@ const analyticsScriptP2 = `
     <QwikSpeakProvider config={config} translationFn={translationFn}>
       <QwikCityProvider>
         <head>
-
-        
           <meta charSet="utf-8" />
           <link rel="manifest" href="/manifest.json" />
-          <meta name="robots" content="noindex"/>
+          {/* <meta name="robots" content="noindex" /> */}
           {/* <link ref="icon" href="/favicon.png" /> */}
           <RouterHead />
+
           <script defer async src={analyticsScriptP1} ></script>
           <script defer async dangerouslySetInnerHTML={analyticsScriptP2}></script>
+          <script defer async dangerouslySetInnerHTML={gtmScriptP1}></script>
 
           <ServiceWorkerRegister />
         </head>
         <body lang="en" class="max-h-screen overflow-x-hidden">
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5BJSW4"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
           <RouterOutlet />
         </body>
       </QwikCityProvider>
