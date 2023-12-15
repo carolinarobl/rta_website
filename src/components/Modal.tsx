2dd5825747df7b2371df9facb88980519a0e5f82
LaloFl <diego.flores@cbluna.com>
2023-12-15 12:02:13 -0800

    feat: zss header

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,9 +4,15 @@ export const Modal = component$(
   ({ showSignal }: { showSignal: Signal<boolean> }) => {
     return (
       <div
-        class={`fixed left-0 right-0 top-0 z-[500] flex h-screen items-center justify-center bg-primary-dark-blue bg-opacity-20 transition-all duration-300 ${
+        class={`fixed left-0 right-0 top-0 z-[500] flex h-screen items-center justify-center bg-primary-dark-blue bg-opacity-30 transition-all duration-300 ${
           showSignal.value ? "" : "hidden"
         }`}
+        onClick$={(e: PointerEvent) => {
+          if (e.target.id === "modalback") {
+            showSignal.value = false;
+          }
+        }}
+        id="modalback"
       >
         <FaXmarkSolid
           class="absolute right-2 top-2 rounded-full bg-secondary-red p-1 text-[30px] font-[200] text-white hover:cursor-pointer"
