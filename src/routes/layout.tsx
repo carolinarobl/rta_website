c219cd34efb5ba9f8fae4128421bb86bcc6cb159
lynatical <carolinaroblero8@gmail.com>
2023-12-14 18:39:38 -0800

    Se deshabilita temporalmente la Splash Screen

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,9 @@
-import { component$, Slot, useSignal, useTask$, useVisibleTask$ } from "@builder.io/qwik";
+import { component$, Slot } from "@builder.io/qwik";
+
+// import { component$, Slot, useSignal, useTask$, useVisibleTask$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
-import { Splashscreen } from "~/components/splashscreen";
+// import { Splashscreen } from "~/components/splashscreen";
 
 // import Header from "~/components/starter/header/header";
 // import Footer from "~/components/starter/footer/footer";
@@ -24,19 +26,20 @@ export const useServerTimeLoader = routeLoader$(() => {
 });
 
 export default component$(() => {
-  const loadingPage = useSignal(true)
-  useTask$(() => {
-    loadingPage.value = true
-  })
-  useVisibleTask$(() => {
-    loadingPage.value = false
-    document.getElementById("splash")?.remove();
-  })
+  // const loadingPage = useSignal(true)
+  // useTask$(() => {
+  //   loadingPage.value = true
+  // })
+  // useVisibleTask$(() => {
+  //   loadingPage.value = false
+  //   document.getElementById("splash")?.remove();
+  // })
   return (
     <>
-      {loadingPage.value 
+      {/* {loadingPage.value 
       ? <div id="splash" class="h-full w-full flex"><Splashscreen /></div> 
-      : <Slot />}
+      : <Slot />} */}
+      <Slot />
     </>
   );
 });
