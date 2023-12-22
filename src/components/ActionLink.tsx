e763ecee5ed37d4125a03d11ed2b6be6766925be
LaloFl <diego.flores@cbluna.com>
2023-12-22 10:38:36 -0800

    fix: /es paths

--- src/components/ActionLink.tsx
+++ src/components/ActionLink.tsx
@@ -1,28 +1,48 @@
-import {Slot, component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { Slot, component$ } from "@builder.io/qwik";
+import { Link, useLocation } from "@builder.io/qwik-city";
 import { PopupCall } from "./PopupCall";
 
 export const ActionLink = component$(
-  ({ link, text = "", classN = "", hasStyle = true }: { link: string, text?: string; classN?: string; hasStyle?: boolean }) => {
+  ({
+    link,
+    text = "",
+    classN = "",
+    hasStyle = true,
+  }: {
+    link: string;
+    text?: string;
+    classN?: string;
+    hasStyle?: boolean;
+  }) => {
+    const location = useLocation();
+    const isSpanish = location.prevUrl?.pathname.includes("/es/");
+    const isPath = link.startsWith("/");
 
-
-    if (link.startsWith("/") || link.startsWith("http") || link.startsWith("tel:"))
+    if (
+      link.startsWith("/") ||
+      link.startsWith("http") ||
+      link.startsWith("tel:")
+    )
       return (
         <Link
           class={classN}
-          href={link}
+          href={(isSpanish && isPath ? "/es" : "") + link}
           prefetch={true}
-          target={link.startsWith("https") ? "_blank" : "_self"}
+          target={link.startsWith("http") ? "_blank" : "_self"}
         >
           <Slot />
         </Link>
       );
     else if (link.startsWith("=") && link) {
-      return <PopupCall link={link} text={text} hasStyle={hasStyle} >  <div class={`${classN} cursor-pointer`}>
-        <Slot></Slot>
-      </div></PopupCall>;
-    }
-    else
+      return (
+        <PopupCall link={link} text={text} hasStyle={hasStyle}>
+          {" "}
+          <div class={`${classN} cursor-pointer`}>
+            <Slot></Slot>
+          </div>
+        </PopupCall>
+      );
+    } else
       return (
         <div class={classN}>
           <Slot></Slot>
