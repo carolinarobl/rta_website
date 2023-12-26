6f08f3a94fff1c4bb8305fdc50710ee27e34b5df
lynatical <carolinaroblero8@gmail.com>
2023-12-26 10:00:29 -0800

    Cambio en página de Zane

--- src/components/pages/ZaneSmithSponsorship/index.tsx
+++ src/components/pages/ZaneSmithSponsorship/index.tsx
@@ -15,9 +15,8 @@ export const ZaneSmithSponsorship = component$(({ data }: { data: any }) => {
       }}
     >
       <SectionHeaderZSS data={pageData} />
-      <div class="bg-gradient-to-b from-[#041630] to-[#153069]">
-        <div class="flex w-full justify-center items-center">
-        <div class="max-w-[1200px] px-8 pt-8">
+      <div class="bg-gradient-to-b from-[#041630] to-[#153069] w-full object-center">
+        <div class="max-w-[1200px] px-8 pt-8 place-items-center">
           <SectionZaneDesc data={pageData} />
           <SectionCarouselZSS data={pageData} />
           <SectionCareerHLZSS data={pageData} />
@@ -25,6 +24,6 @@ export const ZaneSmithSponsorship = component$(({ data }: { data: any }) => {
         </div>
         </div>
       </div>
-    </div>
+
   );
 });
