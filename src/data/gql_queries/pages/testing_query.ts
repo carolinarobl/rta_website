9c8d19bc907b14c79101892777e635cdda8aa6a1
lynatical <carolinaroblero8@gmail.com>
2025-02-01 19:48:34 -0800

    Testing for github mgmt

---
+++ src/data/gql_queries/pages/testing_query.ts
@@ -0,0 +1,24 @@
+import { sectionSEO } from "~/data/constants"
+
+export const texasQuery = (locale:string)=>{
+    return `query{
+        locations(locale:"${locale}", pagination:{limit:500}){
+          data{
+            attributes{
+              Name
+              ZipCode
+              Description
+              Slug
+              office{
+                data{
+                  attributes{
+                    Location
+                  }
+                }
+              }
+              ${sectionSEO}
+            }
+          }
+        }
+      }`
+}
