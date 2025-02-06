fe1e3ad7a51f7e2cc4d26e3f1b5e339c5eeb9be1
lynatical <carolinaroblero8@gmail.com>
2025-02-05 21:40:45 -0800

    Config for production build

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,11 +1,10 @@
 import { type DocumentHeadValue } from "@builder.io/qwik-city";
 
-export const strapiURL = "https://strapi42.rtatel.com";
 // Strapi por dominio
-// export const strapiURL = "https://strapi.rtatel.com";
+export const strapiURL = "https://strapi.rtatel.com";
 
 // Strapi por ip interna
-export const strapiGQLURL = "http://10.5.24.42:1337";
+export const strapiGQLURL = "http://10.5.24.41:1337";
 
 export const gqlURL = `${strapiGQLURL}/graphql`;
 
--- src/root.tsx
+++ src/root.tsx
@@ -13,26 +13,28 @@ import { translationFn } from "./speak-functions";
 import "./global.css";
 
 export default component$(() => {
-//   const analyticsScriptP1 = `https://www.googletagmanager.com/gtag/js?id=G-73FN8JZHHH`;
+  const analyticsScriptP1 = `https://www.googletagmanager.com/gtag/js?id=G-19EN0CY127`;
 
-// const analyticsScriptP2 = `
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-//   gtag('config', 'G-73FN8JZHHH');
-// `;
+  const analyticsScriptP2 = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-19EN0CY127');
+  `;
 
-// const pixelScript = `
-// !function(f,b,e,v,n,t,s)
-//   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-//   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-//   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-//   n.queue=[];t=b.createElement(e);t.async=!0;
-//   t.src=v;s=b.getElementsByTagName(e)[0];
-//   s.parentNode.insertBefore(t,s)}(window, document,'script',
-//   'https://connect.facebook.net/en_US/fbevents.js');
-//   fbq('init', '427875699033893');
-//  `;
+  const pixelScript = `
+  !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '427875699033893');
+    fbq('track', 'PageView');
+   `;
+   
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
@@ -48,21 +50,23 @@ export default component$(() => {
         
           <meta charSet="utf-8" />
           <link rel="manifest" href="/manifest.json" />
-          <meta name="robots" content="noindex"/> 
+          <meta name="robots" content="index"/> 
           <link rel="icon" href="/favicon.ico" />
           <RouterHead />
 
-          {/* <script defer async dangerouslySetInnerHTML={pixelScript}></script> */}
-          
-          {/* <script defer async src={analyticsScriptP1} ></script>
-          <script defer async dangerouslySetInnerHTML={analyticsScriptP2}></script> */}
-
+          <script defer async src={analyticsScriptP1} ></script>
+          <script defer async dangerouslySetInnerHTML={analyticsScriptP2}></script>
+          {/* <script defer async dangerouslySetInnerHTML={gtmScriptP1}></script> */}
+          <script defer async dangerouslySetInnerHTML={pixelScript}></script>
           <ServiceWorkerRegister />
         </head>
         <body lang="en" class="max-h-screen overflow-x-hidden">
-        {/* <noscript><img height="1" width="1" style="display:none"
+        <noscript><img height="1" width="1" style="display:none"
   src="https://www.facebook.com/tr?id=427875699033893&ev=PageView&noscript=1"
-/></noscript> */}
+/></noscript>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5BJSW4"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
           <RouterOutlet />
         </body>
       </QwikCityProvider>
